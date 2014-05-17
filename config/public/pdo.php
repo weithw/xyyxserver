620d611520103e11a1b45feb1e77c50a4d3a6083
weithw <llghwll@qq.com>
2014-05-17 10:07:04 +0800

    refresh group list

--- config/public/pdo.php
+++ config/public/pdo.php
@@ -4,7 +4,7 @@
             'dsn' => 'mysql:host=127.0.0.1;dbname=xyyx',
             'name' => 'cd',
             'user' => 'root',
-            'pass' => '89657415',
+            'pass' => '123456',
             'dbname' => 'xyyx',
             'charset' => 'UTF8',
             'pconnect'=>true,
